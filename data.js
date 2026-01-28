70281eff968477032b6bb496be46b4e650f8d1b1
Videograf <videografs@mail.ru>
2026-01-28 15:19:08 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,353 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--2",
+      "name": "Коридор 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.5331237411057863,
+        "pitch": 0.32229743090911356,
+        "fov": 1.4084158071147468
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.517637553356943,
+          "pitch": 0.14673219403998417,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.9411902852395926,
+          "pitch": 0.039787559069429435,
+          "rotation": 0,
+          "target": "1--1"
+        },
+        {
+          "yaw": 0.6938712801045064,
+          "pitch": 0.13290583306520887,
+          "rotation": 0,
+          "target": "4--1"
+        },
+        {
+          "yaw": 0.1683635971902646,
+          "pitch": 0.13632970389603116,
+          "rotation": 10.995574287564278,
+          "target": "3--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--1",
+      "name": "Комната 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.269023648064671,
+        "pitch": 0.15938153381159026,
+        "fov": 1.4084158071147468
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.372683265754187,
+          "pitch": 0.1452970841716006,
+          "rotation": 0,
+          "target": "0--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Гостиная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.7455030622374075,
+        "pitch": 0.2091654311785689,
+        "fov": 1.4084158071147468
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1121821966088845,
+          "pitch": 0.1238341824490714,
+          "rotation": 0,
+          "target": "4--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--2",
+      "name": "Комната 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.4914654996438896,
+        "pitch": 0.2519310784465585,
+        "fov": 1.4084158071147468
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.104448662042838,
+          "pitch": 0.11067783155321109,
+          "rotation": 0,
+          "target": "4--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--1",
+      "name": "Коридор 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.9259835079440144,
+        "pitch": 0.071643216518531,
+        "fov": 1.4084158071147468
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7068298346689765,
+          "pitch": 0.12571741606219966,
+          "rotation": 0,
+          "target": "0--2"
+        },
+        {
+          "yaw": 2.914876972522457,
+          "pitch": 0.062039185925574,
+          "rotation": 0,
+          "target": "5--3"
+        },
+        {
+          "yaw": -0.12795852802447705,
+          "pitch": 0.06232231208149486,
+          "rotation": 0,
+          "target": "3--2"
+        },
+        {
+          "yaw": 1.0190291089098924,
+          "pitch": 0.0582410301442593,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -1.180129358598215,
+          "pitch": 0.10538459740125283,
+          "rotation": 1.5707963267948966,
+          "target": "1--1"
+        },
+        {
+          "yaw": -2.1610109123726,
+          "pitch": 0.09128572776454114,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--3",
+      "name": "Комната 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.565982195111479,
+        "pitch": 0.12362760135160222,
+        "fov": 1.4084158071147468
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5109351505152944,
+          "pitch": 0.14767337163751826,
+          "rotation": 0,
+          "target": "4--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "Санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.220342290216001,
+        "pitch": 0.6167887633472411,
+        "fov": 1.4084158071147468
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7163705713942434,
+          "pitch": 0.22108340060677634,
+          "rotation": 6.283185307179586,
+          "target": "4--1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Дом в Дзержинске360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
